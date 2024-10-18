6d7c94be388bc952210d1e427f3ba9fa75391676
valhugnat1 <hugo.philipp99@gmail.com>
2024-10-18 10:56:39 +0200

    formating + first lunary test

--- rag/server_langchain.py
+++ rag/server_langchain.py
@@ -2,40 +2,47 @@
 
 import asyncio
 import json
+import os  # Importing os module for operating system functionalities
+import shutil  # Importing shutil module for high-level file operations
 import time
+from typing import List, Optional
 
-from typing import Optional, List
-
-from pydantic import BaseModel, Field
-
-from starlette.responses import StreamingResponse
+from dotenv import load_dotenv  # Importing dotenv to get API key from .env file
 from fastapi import FastAPI, HTTPException, Request
-from langchain.document_loaders.pdf import PyPDFDirectoryLoader # Importing PDF loader from Langchain
-from langchain.text_splitter import RecursiveCharacterTextSplitter # Importing text splitter from Langchain
-from langchain.embeddings import OpenAIEmbeddings # Importing OpenAI embeddings from Langchain
-from langchain.schema import Document # Importing Document schema from Langchain
-from langchain.vectorstores.chroma import Chroma # Importing Chroma vector store from Langchain
-from dotenv import load_dotenv # Importing dotenv to get API key from .env file
-from langchain.chat_models import ChatOpenAI # Import OpenAI LLM
-import os # Importing os module for operating system functionalities
-import shutil # Importing shutil module for high-level file operations
+from fastapi.middleware.cors import CORSMiddleware
+from langchain.chat_models import ChatOpenAI  # Import OpenAI LLM
+from langchain.document_loaders.pdf import (
+    PyPDFDirectoryLoader,
+)  # Importing PDF loader from Langchain
+from langchain.embeddings import (
+    OpenAIEmbeddings,
+)  # Importing OpenAI embeddings from Langchain
+from langchain.schema import Document  # Importing Document schema from Langchain
+from langchain.text_splitter import (
+    RecursiveCharacterTextSplitter,
+)  # Importing text splitter from Langchain
+from langchain.vectorstores.chroma import (
+    Chroma,
+)  # Importing Chroma vector store from Langchain
 from langchain_community.document_loaders import TextLoader
 from langchain_core.prompts import ChatPromptTemplate
-
-from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
 from starlette.middleware.base import BaseHTTPMiddleware
-
+from starlette.responses import StreamingResponse
 
 # Path to the directory to save Chroma database
 CHROMA_PATH = "chroma"
+import logging
 import os
 
 from dotenv import load_dotenv
-import os
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import Response
 
-import logging
+# from langchain_openai import ChatOpenAI
+# from lunary import LunaryCallbackHandler
+# handler = LunaryCallbackHandler()
+
 
 # Configure the logging system (you can customize this part to suit your needs)
 logging.basicConfig(level=logging.INFO)
@@ -46,9 +53,7 @@ load_dotenv()  # This loads the environment variables from the .env file
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 
-
 ### data model ###
-
 
 
 PROMPT_TEMPLATE = """
@@ -72,8 +77,6 @@ class ChatCompletionRequest(BaseModel):
     stream: Optional[bool] = False
 
 
-
-
 app = FastAPI(title="OpenAI-compatible API")
 
 
@@ -83,7 +86,9 @@ class LogIncorrectPathsMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
         if response.status_code == 404:
             # Log the request path and other details
-            print(f"Incorrect endpoint path: {request.url.path}, Method: {request.method}")
+            print(
+                f"Incorrect endpoint path: {request.url.path}, Method: {request.method}"
+            )
             # You can add more logging details if needed
         return response
 
@@ -91,7 +96,9 @@ class LogIncorrectPathsMiddleware(BaseHTTPMiddleware):
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins, replace "*" with specific domains if needed
+    allow_origins=[
+        "*"
+    ],  # Allows all origins, replace "*" with specific domains if needed
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
@@ -101,46 +108,54 @@ app.add_middleware(
 ### Function ###
 
 
-def context_rag_fct (query_text): 
-  embedding_function = OpenAIEmbeddings()
-  db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+def context_rag_fct(query_text):
+    # embedding_function = OpenAIEmbeddings()
+    embedding_function = OpenAIEmbeddings(
+        openai_api_base="https://api.scaleway.ai/v1",
+        openai_api_key=os.getenv("SCW_SECRET_KEY"),
+    )
 
-  results = db.similarity_search_with_relevance_scores(query_text, k=10)
-  if len(results) == 0 or results[0][1] < 0.7:
-    print(f"Unable to find matching results.")
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-  context_text = "\n\n - -\n\n".join([doc.page_content for doc, _score in results])
-  prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-  prompt = prompt_template.format(context=context_text, question=query_text)
+    results = db.similarity_search_with_relevance_scores(query_text, k=10)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
 
-  return prompt, results
+    context_text = "\n\n - -\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
 
+    return prompt, results
 
 
 def query_rag(query_text):
-  
-  prompt, results = context_rag_fct(query_text)
-  
-  model = ChatOpenAI(model="gpt-4o-mini")
-  
-  response_text = model.predict(prompt)
-  sources = [doc.metadata.get("source", None) for doc, _score in results]
-  formatted_response = f"Response: {response_text}\nSources: {sources}"
-  return formatted_response, response_text
+    prompt, results = context_rag_fct(query_text)
 
+    model = ChatOpenAI(
+        openai_api_base="https://api.scaleway.ai/v1",
+        openai_api_key=os.getenv("SCW_SECRET_KEY"),
+        model_name="llama-3.1-8b-instruct",
+    )  # , callbacks=[handler]
 
+    response_text = model.predict(prompt)
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    return formatted_response, response_text
 
 
 async def _resp_async_generator(request: str):
-  
     # Enable streaming in the model call
-    model = ChatOpenAI(model="gpt-4o-mini", streaming=True)
+    model = ChatOpenAI(
+        openai_api_base="https://api.scaleway.ai/v1",
+        openai_api_key=os.getenv("SCW_SECRET_KEY"),
+        model_name="llama-3.1-8b-instruct",
+        streaming=True,
+    )  # , callbacks=[handler]
 
     prompt, results = context_rag_fct(str(request.messages[-1]))
 
-    i = 0 
+    i = 0
     for chunk in model.stream(prompt):
-        
         response_chunk = chunk.content
 
         chunk = {
@@ -152,7 +167,7 @@ async def _resp_async_generator(request: str):
         }
         i += 1
         yield f"data: {json.dumps(chunk)}\n\n"
-        
+
     yield "data: [DONE]\n\n"
 
 
@@ -166,10 +181,10 @@ app.add_middleware(LogIncorrectPathsMiddleware)
 @app.middleware("http")
 async def set_referrer_policy(request: Request, call_next):
     response = await call_next(request)
-    
+
     # Add the Referrer-Policy header
     response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
-    
+
     return response
 
 
@@ -178,10 +193,10 @@ async def set_referrer_policy(request: Request, call_next):
 async def log_query_params(request: Request, call_next):
     # Get the full URL and query parameters
     query_params = dict(request.query_params)
-    
+
     # Log the query parameters
     logging.info(f"Request URL: {request.url} - Query params: {query_params}")
-    
+
     # Proceed with the request
     response = await call_next(request)
     return response
@@ -189,21 +204,18 @@ async def log_query_params(request: Request, call_next):
 
 #### Route ####
 
- 
+
 @app.post("/chat/completions")
 async def chat_completions(request: ChatCompletionRequest):
     if request.messages:
-
         _, response_text = query_rag(str(request.messages[-1]))
 
         if request.stream:
-
-
             return StreamingResponse(
                 _resp_async_generator(request), media_type="text/event-stream"
             )
 
-        else : 
+        else:
             _, response_text = query_rag(str(request.messages[-1]))
 
             return {
@@ -211,25 +223,27 @@ async def chat_completions(request: ChatCompletionRequest):
                 "object": "chat.completion",
                 "created": time.time(),
                 "model": request.model,
-                "choices": [{"message": Message(role="assistant", content=str(response_text))}],
+                "choices": [
+                    {"message": Message(role="assistant", content=str(response_text))}
+                ],
             }
 
 
 @app.get("/models")
 async def models_list():
-
     return {
         "object": "list",
         "data": [
             {
-            "id": "llama-3.1-8b-rag",
-            "object": "model",
-            "created": 1686935008,
-            "owned_by": "organization-owner"
+                "id": "llama-3.1-8b-rag",
+                "object": "model",
+                "created": 1686935008,
+                "owned_by": "organization-owner",
             },
         ],
-        "object": "list"
-        }
+        "object": "list",
+    }
+
 
 @app.get("/health")
 def health_check():
@@ -241,15 +255,11 @@ def read_root():
     return {"message": "Hello, World!"}
 
 
-
-
-
 ### Main ###
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.getenv("PORT", 8000))  
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
-
